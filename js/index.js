c42e7dec1720dc13542739006819b16ac730143b
Emanuel Rangel Luna <emanuelrangelluna@gmail.com>
2020-06-17 15:52:13 -0500

    index

--- js/index.js
+++ js/index.js
@@ -9,15 +9,26 @@ formulario.addEventListener('submit', login);
 //funciones a ejecutar
 function login(e){
     e.preventDefault();
-    console.log('Se ejecuta evento de form');
+    
     let usuarioVal = usuario.value;
     let passwordVal = password.value;
 
     if(usuarioVal == '' || passwordVal =='' ){
+        console.log ('Verifica tus campos')
+        creaMensaje('Verifica tus campos', 'danger');
         return;
     }
 
-    console.log('el valor para user es:' + usuarioVal);
-    console.log('el valor para password es:' + passwordVal);
+  if(localStorage.getItem('usuario')){
+      let objeto = JSON.parse(localStorage.getItem('usuario'));
+
+      if(usuarioVal == objeto.user && passwordVal == objeto.pass){
+          creaMensaje('Login Correcto', 'success');
+      }else{
+          creaMensaje('Usuario Incorrecto', 'danger');
+      }
+    }else {
+        creaMensaje('No hay registros', 'danger');
+    }
 
 }
